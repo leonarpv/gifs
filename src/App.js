3066af1737601188a108a952a085780a3df1eb46
Leonar P <lperez@auntap.com>
2020-11-21 20:03:06 -0300

    feat (react-query) remove react query

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 import React, { Fragment, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { ReactQueryConfigProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query-devtools"; // Dev tools f
 import { Home, Search, Detail } from "./pages";
-
-const overrides = {
-  queries: {
-    suspense: false,
-  },
-  mutations: {
-    suspense: false,
-  },
-};
 
 function App() {
   return (
-    <ReactQueryConfigProvider config={overrides}>
-      <Suspense fallback={null}>
-        <BrowserRouter>
-          <Fragment>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/search/:query" component={Search} />
-              <Route path="/gif/:id" component={Detail} />
-            </Switch>
-          </Fragment>
-          <ReactQueryDevtools initialIsOpen={false} />
-        </BrowserRouter>
-      </Suspense>
-    </ReactQueryConfigProvider>
+    <Suspense fallback={null}>
+      <BrowserRouter>
+        <Fragment>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/search/:query" component={Search} />
+            <Route path="/gif/:id" component={Detail} />
+          </Switch>
+        </Fragment>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
