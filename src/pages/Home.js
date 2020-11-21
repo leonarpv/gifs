6cdcedee6626f2720bd0c5042a22d3d5fce22bee
Leonar P <lperez@auntap.com>
2020-11-21 15:58:36 -0300

    feat (home) fetch trending gifs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
 import React, { useState, useEffect, useCallback } from "react";
 import useTrending from "../hooks/useTrending";
+import { Link } from "react-router-dom";
+
 export const Home = ({ ...rest }) => {
   const { isFetching, isError, data, refetch } = useTrending();
 
-  return <div>{isFetching ? <div>Cargando</div> : <div>Tengo data</div>}</div>;
+  return (
+    <div>
+      {isFetching && <div>Cargando</div> && console.log("cargo")}
+      {isError && <div>Ocurió un error</div>}
+      {!isFetching && (
+        <ul>
+          {data.map((gif) => (
+            <li>
+              <Link to={`/gif/${gif.id}`}>{gif.title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
