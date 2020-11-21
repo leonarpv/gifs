97d4be022ccd13b19b6a0faf3dfc04dea2c40b67
Leonar P <lperez@auntap.com>
2020-11-21 15:15:31 -0300

    feat (pages) setting up right names and hooks by screens

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
-import useTrending from "../hooks/useTrending";
-export const Home = ({ ...rest }) => {
+import useGif from "../hooks/useTrending";
+export const Detail = ({ ...rest }) => {
   const { isFetching, isError, data, refetch } = useGif();
 
   return <div>{isFetching ? <div>Cargando</div> : <div>Tengo data</div>}</div>;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from "react";
 
 import useSearchGifs from "../hooks/useSearchGifs";
 
-export const Home = ({ ...rest }) => {
+export const Search = ({ ...rest }) => {
   const { isFetching, isError, data, refetch } = useSearchGifs();
 
   return <div>{isFetching ? <div>Cargando</div> : <div>Tengo data</div>}</div>;
