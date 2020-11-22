e7f763f9c6275fd1ee2fc37fecc5b9b9402d4690
Leonar P <lperez@auntap.com>
2020-11-22 19:45:59 -0300

    fix (styles) remove bootstrap

--- src/components/common/SearchBar/SearchBox/styled.js
+++ src/components/common/SearchBar/SearchBox/styled.js
@@ -2,19 +2,30 @@ import styled from "styled-components";
 
 const StyledSearchBox = styled.div`
   display: flex;
-  flex-direction: row;
-  align-self: center;
+  flex-direction: column;
+  background: #effffa;
+  width: 100%;
+  border-radius: 5px;
+  position: relative;
+  box-shadow: 0px 1px 5px 3px rgba(0, 0, 0, 0.12);
 `;
 const StyledSearchForm = styled.form`
   width: 100%;
 `;
 const StyledSearchInput = styled.input`
-  justify-content: stretch;
-
-  font-size: 22px;
-  border: unset;
+  height: 55px;
+  background: #effffa;
+  width: 100%;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  padding: 0 60px 0 20px;
+  font-size: 18px;
+  .active {
+    border-radius: 5px 5px 0 0;
+  }
   :focus {
-    border: unset;
+    border-color: unset;
   }
 `;
 
