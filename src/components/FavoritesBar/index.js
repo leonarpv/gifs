747d8555b108741b148ea9a88824b0202f9bb916
Leonar P <lperez@auntap.com>
2020-11-23 00:56:37 -0300

    feat (favorites bar) title list outside of hoc

--- src/components/FavoritesBar/index.js
+++ src/components/FavoritesBar/index.js
@@ -1,19 +1,17 @@
 import React from "react";
-
 import { useUserState } from "../../context/UserContext/hooks";
 import { Sidebar } from "../common";
-
 import FavoritesList from "./FavoritesList";
-
+import { ListTitle } from "./styled";
 export const FavoritesBar = ({ open }) => {
   const { favorites } = useUserState();
   return (
     <Sidebar open={open}>
+      <ListTitle>Your favorites</ListTitle>
       <FavoritesList
         height="150"
         width="150"
         itemNoTitle
-        title="Your favorites"
         favorites={favorites}
       />
     </Sidebar>
---
+++ src/components/FavoritesBar/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+const ListTitle = styled.h1`
+  font-size: 48px;
+  color: #0d0c1d;
+`;
+export { ListTitle };
