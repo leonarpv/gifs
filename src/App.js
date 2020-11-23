8fa36ae26923e18c718ce4faf15f6a10ac6fa25c
Leonar P <lperez@auntap.com>
2020-11-23 00:26:15 -0300

    feat (lint) fixing lint details and remove of theme context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Fragment, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import GifContext from "./context/GifContext/GifContext";
-import ThemeContext from "./context/ThemeContext/ThemeContext";
 import UserContext from "./context/UserContext/UserContext";
 import { Home, Search, Detail } from "./pages";
 
@@ -9,19 +8,17 @@ function App() {
   return (
     <UserContext>
       <GifContext>
-        <ThemeContext>
-          <Suspense fallback={null}>
-            <BrowserRouter>
-              <Fragment>
-                <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route path="/search/:query?" component={Search} />
-                  <Route path="/gif/:id" component={Detail} />
-                </Switch>
-              </Fragment>
-            </BrowserRouter>
-          </Suspense>
-        </ThemeContext>
+        <Suspense fallback={null}>
+          <BrowserRouter>
+            <Fragment>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/search/:query?" component={Search} />
+                <Route path="/gif/:id" component={Detail} />
+              </Switch>
+            </Fragment>
+          </BrowserRouter>
+        </Suspense>
       </GifContext>
     </UserContext>
   );
