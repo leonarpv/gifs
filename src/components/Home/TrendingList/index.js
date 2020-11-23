09459f9334815f91f0b41b6507c36803ab2e7365
Leonar P <lperez@auntap.com>
2020-11-22 23:50:47 -0300

    feat (search page) adding search with loading

--- src/components/Home/TrendingList/index.js
+++ src/components/Home/TrendingList/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import GlobalLoader from "../../common/GlobalLoader";
 import ItemList from "../../common/ItemList";
 
-const ListWithLoading = GlobalLoader(ItemList);
+const TrendingListWithLoading = GlobalLoader(ItemList);
 
 export default function TrendingList({ trendings = [], loading, ...rest }) {
-  return <ListWithLoading isLoadings={loading} items={trendings} {...rest} />;
+  return (
+    <TrendingListWithLoading isLoadings={loading} items={trendings} {...rest} />
+  );
 }
--- src/components/Search/SearchResults/index.js
+++ src/components/Search/SearchResults/index.js
@@ -1,6 +1,11 @@
 import React from "react";
+import GlobalLoader from "../../common/GlobalLoader";
 import ItemList from "../../common/ItemList";
 
-export default function SearchResults({ results = [], ...rest }) {
-  return <ItemList items={results} {...rest} />;
+const SearchResultsWithLoading = GlobalLoader(ItemList);
+
+export default function SearchResults({ results = [], loading, ...rest }) {
+  return (
+    <SearchResultsWithLoading isLoadings={loading} items={results} {...rest} />
+  );
 }
